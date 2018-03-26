2cd30868329f666a91acd6fe06d888f9439926cc
jsnanigans <megamaaaan@gmail.com>
2018-03-26 22:47:03 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,25 @@
+qrme
+====
+
+Generate qr to share file or directory with mobile phone
+
+[![Version](https://img.shields.io/npm/v/qrme.svg)](https://npmjs.org/package/qrme)
+[![CircleCI](https://circleci.com/gh/jsnanigans/qrme/tree/master.svg?style=shield)](https://circleci.com/gh/jsnanigans/qrme/tree/master)
+[![Appveyor CI](https://ci.appveyor.com/api/projects/status/github/jsnanigans/qrme?branch=master&svg=true)](https://ci.appveyor.com/project/jsnanigans/qrme/branch/master)
+[![Codecov](https://codecov.io/gh/jsnanigans/qrme/branch/master/graph/badge.svg)](https://codecov.io/gh/jsnanigans/qrme)
+[![Downloads/week](https://img.shields.io/npm/dw/qrme.svg)](https://npmjs.org/package/qrme)
+[![License](https://img.shields.io/npm/l/qrme.svg)](https://github.com/jsnanigans/qrme/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+```bash
+qrme {file}
+```
+<!-- commands -->
+
+
+# Acknowledgements
+The original: https://github.com/claudiodangelis/qr-filetransfer
+this is just a atempt to do the same with node
---
+++ src/index.ts
@@ -0,0 +1,156 @@
+import {Command, flags} from '@oclif/command'
+import * as QRCode from 'qrcode'
+const os = require('os')
+
+// const crypto = require('crypto')
+// const fs = require('fs')
+import * as fs from 'fs'
+import * as http from 'http'
+import * as path from 'path'
+
+// class Qrme extends Command {
+//   static description = 'describe the command here'
+
+//   static examples = [
+//     `$ qrme
+// hello world from ./src/qrme.ts!
+// `,
+//   ]
+
+//   static flags = {
+//     // add --version flag to show CLI version
+//     version: flags.version({char: 'v'}),
+//     // add --help flag to show CLI version
+//     help: flags.help({char: 'h'}),
+
+//     // flag with a value (-n, --name=VALUE)
+//     name: flags.string({char: 'n', description: 'name to print'}),
+//     force: flags.boolean({char: 'f'}),
+//   }
+
+//   static args = [{name: 'file'}]
+
+//   async run() {
+//     const {args, flags} = this.parse(Qrme)
+
+//     const name = flags.name || 'world'
+//     this.log(`hello ${name} from ${__filename}!`)
+//     if (args.file && flags.force) {
+//       this.log(`you input --force and --file: ${args.file}`)
+//     }
+//   }
+// }
+
+const stringQR = (string: string) => {
+  return new Promise((resolve, reject) => {
+    QRCode.toString(string, function (err: Error, string: string) {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve(string)
+    })
+  })
+}
+
+const startServer = (options: any = {}) => {
+  return new Promise(() => {
+    const handler = function (req: any, res: any) {
+      // res.writeHead(200, {'Content-Type': 'text/plain'})
+      // res.end('Hello World\n')
+      res.writeHead(200, {
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': 'attachment; filename=' + options.file
+      })
+      const readStream = fs.createReadStream(options.filePath)
+      readStream.pipe(res)
+    }
+
+    const server = http.createServer()
+    server.addListener('request', handler)
+    server.listen(options.port)
+
+  })
+}
+
+const checkFile = (file: string) => {
+  const rt: any = {}
+  const filePath = path.join(__dirname, file)
+
+  const exists = fs.existsSync(filePath)
+  if (!exists) {
+    rt.error = 'File does not exists'
+  }
+
+  if (!rt.error) {
+    // console.log(fs.statSync(filePath))
+  }
+
+  rt.filePath = filePath
+  return rt
+}
+
+const getLocalIp = (): string => {
+  let ifaces = os.networkInterfaces()
+  let ip: string = ''
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    let alias = 0
+
+    ifaces[ifname].forEach(function (iface: any) {
+      if (iface.family !== 'IPv4' || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return
+      }
+
+      // return iface.address
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        ip = iface.address
+      } else {
+        // this interface has only one ipv4 adress
+        ip = iface.address
+      }
+      ++alias
+    })
+  })
+
+  return ip
+}
+
+class Qrme extends Command {
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(Qrme)
+    const {file} = args
+
+    const port = 8899
+    const ip = getLocalIp()
+    const url = `http://${ip}:${port}`
+
+    const {meta, error, filePath} = checkFile(file)
+    if (error) {
+      this.error(error)
+      return
+    }
+
+    this.log(meta)
+
+    // this.log(`Serving ${file} at ${url}`)
+
+    const qrCode = await stringQR(url)
+    this.log(qrCode)
+
+    await startServer({
+      indexFile: file,
+      host: ip,
+      port,
+      file,
+      filePath
+    })
+  }
+}
+
+export = Qrme
